64887758228f7fab7dad8a4922fd7fadd0a29573
Austin Schneider <soccer022483@gmail.com>
2011-01-27 16:35:41 -0500

    fix typo

--- config/compass.rb
+++ config/compass.rb
@@ -10,5 +10,5 @@ environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 if %w(staging production).include?(Rails.env)
-  sass_options[:never_update] = true
+  sass_options = {:never_update => true}
 end
